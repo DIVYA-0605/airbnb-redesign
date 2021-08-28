967fea5e4b5ac2f092849bee018a1660bd3e1a18
DIVYA525-VAD <68990221+DIVYA525-VAD@users.noreply.github.com>
2021-08-28 08:37:44 +0530

    header update

--- components/Header.js
+++ components/Header.js
@@ -9,9 +9,9 @@ import {
 
 function Header() {
   return (
-    <header className="sticky top-0 z-50 bg-white shadow-md p-5 md:px-10 grid grid-cols-3 ">
+    <header className="sticky top-0 z-50 bg-white shadow-md p-5 md:px-10 md:grid md:grid-cols-3">
       {/* left */}
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div className="relative md:flex hidden items-center h-10 cursor-pointer my-auto">
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
@@ -21,18 +21,18 @@ function Header() {
       </div>
 
       {/* middle Search*/}
-      <div className="flex items-center rounded-full md:shawdow-sm   md:border-2  py-2">
+      <div className="flex items-center rounded-full shawdow-sm  border-2  lg:p-3 p-2 justify-between">
         <input
           type="text"
           placeholder="Start your Search"
-          className="outline-none  pl-5 bg-transparent flex-grow text-small text-gray-600 placeholder-gray-400"
+          className="outline-none   bg-transparent  text-small text-gray-600 placeholder-gray-400"
         />
-        <SearchIcon className="h-8 bg-red-400 rounded-full text-white p-2 cursor-pointer hidden md:inline-flex md:mx-2" />
+        <SearchIcon className="h-8 bg-red-400 rounded-full text-white p-2 cursor-pointer   flex-shrink-0 " />
       </div>
 
       {/* right */}
-      <div className="flex space-x-4 items-center justify-end text-gray-500">
-        <p className="hidden md:inline cursor-pointer">Become a host</p>
+      <div className="md:flex hidden space-x-4 items-center justify-end text-gray-500">
+        <p className="hidden lg:inline cursor-pointer">Become a host</p>
         <GlobeAltIcon className="h-6 cursor-pointer" />
 
         <div className="flex rounded-full border-2 items-center space-x-2 p-2">
