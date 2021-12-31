561dd1e00a978b77c00473d868a29ceee4e86548
DIVYA525-VAD <68990221+DIVYA525-VAD@users.noreply.github.com>
2021-12-31 12:30:12 +0530

    airbnb

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ module.exports={
         domains:["links.papareact.com"],
 
     },
+    env:{
+        mapbox_key:'pk.eyJ1IjoiZGl2eWEtMjUiLCJhIjoiY2t4dDN4bnZiMzdpbTJxcWtjOHFncDVmYyJ9.R_JRtla5_R4uSXwnQu9lZQ'
+    }
 };
